466aaaeae37c5bef49f29b59259beb34de5dcf16
yanfer <yanfer.daniel@gmail.com>
2023-12-05 12:29:42 -0600

    Prima confing and getServerSession

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,13 @@
-export default function Home() {
+import { getServerSession } from 'next-auth';
+import { authOptions } from './api/auth/[...nextauth]/route';
+
+export default async function Home() {
+  const session = await getServerSession(authOptions);
   return (
     <div>
       <h1>Home</h1>
+      <h1>Server Side Rendered</h1>
+      <pre>{JSON.stringify(session)}</pre>
     </div>
   );
 }
